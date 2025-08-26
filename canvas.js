0341b025081bb66bb1d601e9ed96062515237901
salmaanjhetam <42811395+salmaanjhetam@users.noreply.github.com>
2025-08-26 16:20:35 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -5,4 +5,18 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
 
 c.fillStyle = "blue";
-c.fillRect(50, 50, 50, 50);
+
+var rectX = 50;
+var rectY = 50;
+var rectSpeedX = 2;
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillRect(rectX, rectY, 50, 50);
+  
+  if(rectX+50 >= window.innerWidth)
+    rectSpeedX = -2;
+  else if(rectX <= 0)
+    rectSpeedX = 2;
+
+  rectX += rectSpeedX;
+}
