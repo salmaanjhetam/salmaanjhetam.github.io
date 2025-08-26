a6a222a2e96e03fd5652c35b7e2bd2170b4cfa20
salmaanjhetam <42811395+salmaanjhetam@users.noreply.github.com>
2025-08-26 15:58:13 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,3 +1,7 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+c.fillRect(50, 50, 50, 50);
